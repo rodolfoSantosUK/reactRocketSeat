6529893959688b73d06f26d4083015e60dbfb728
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-01 19:53:00 -0300

    [ADD] Uso do componentDidMount, array function para não alterar o valor do this.

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
-
+import Main from './pages/main/index'
 import './styles.css'
 
 const App = () => (
   <div className="App">
    <Header/>
+   <Main/>
   </div>
 
 );
