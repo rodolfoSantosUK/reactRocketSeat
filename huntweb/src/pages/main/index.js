08e7c664f9bc3249733ab8c995d8e0bab9d2b474
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-02 15:09:12 -0300

    [ADD] Nesse commit fizemos a paginação

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css'
 
 export default class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1
     }
 
     // Esse método é chamado assim que o componente é iniciado
@@ -12,25 +15,53 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products')
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`)
+        const { docs, ...productInfo } = response.data;
+        this.setState({ products: docs, productInfo, page })
         console.log(response.data.docs);
-
-        this.setState({ products: response.data.docs })
-
     };
 
 
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+        if (page === 1) return;
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+        // Se estiver na ultima página
+        if (page === productInfo.pages) return;
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    }
+
+
     render() {
-        return (<> <h1>Contagem de produtos: {this.state.products.length}</h1>
 
-            <div className="product-list">
-                {this.state.products.map(product => (
-                    <h2 key={product._id} >{product.title}</h2>
-                ))}
+        // processo de desestruturação
+        const { products, page, productInfo } = this.state
 
-            </div>
-        </>
+        return (<> <h1> Total de produtos: {this.state.products.length} </h1>
+
+            <div className="product-list" > {
+                products.map(product => (
+                    <article key={product._id} >
+                        <strong> {product.title} </strong>
+                        <p> {product.description} </p>
+                        <a href="" > Acessar </a>
+                    </article >
+                ))
+            }
+                <div className="actions">
+                    <button disabled={page===1} onClick={this.prevPage} >Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage} >Próximo</button>
+                </div>
+
+            </div> </>
         );
 
 
